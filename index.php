30f95b7185d6d15cee9c097d1861b130bea7df6e
nicksonthecypher <nicksonthecypher@gmail.com>
2015-11-14 23:01:55 -0800

    Cleaning code

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 	error_reporting(-1);
 	ini_set('display_errors', 'On');
+	
 	/**
 	* phpNgin - simple PHP framework
 	*/
+
 	$site_path = realpath(dirname(__FILE__));
 	define ('__SITE_PATH', $site_path);
 	use App\Ngin\Handler as Handler;
